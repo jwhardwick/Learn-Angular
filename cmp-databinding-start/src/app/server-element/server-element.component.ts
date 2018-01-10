2708c9c5063b33ccfc911b27e3579fba7bde22d6
James Hardwick <jwhardwick@gmail.com>
2018-01-10 11:00:27 +1100

    Finishes Section 5.

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,58 +1,41 @@
-import { Component, OnInit, Input, OnChanges, DoCheck, AfterContentInit, AfterViewInit } from '@angular/core';
-import { SimpleChanges, AfterContentChecked, AfterViewChecked, OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterViewInit,
+  SimpleChanges,
+  AfterContentChecked,
+  AfterViewChecked,
+  OnDestroy,
+  ViewChild,
+  ElementRef,
+  ContentChild
+} from '@angular/core';
+
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements
-    OnInit,
-    OnChanges,
-    DoCheck,
-    AfterContentInit,
-    AfterContentChecked,
-    AfterViewInit,
-    AfterViewChecked,
-    OnDestroy {
+export class ServerElementComponent implements OnInit {
 
   @Input() element: {type: string, name: string, content: string};
   @Input() name: string;
+
+  @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
 
   constructor() {
     console.log('constructor called');
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    console.log(changes);
-  }
-
   ngOnInit() {
     console.log('ngOnInit called');
-  }
-
-  ngDoCheck() {
-    console.log('ngDoCheck()');
-  }
-
-  ngAfterContentInit() {
-    console.log('ngAfterContentInit()');
-  }
-
-  ngAfterContentChecked() {
-    console.log('ngAfterContentCheck()');
-  }
-
-  ngAfterViewInit() {
-    console.log('ngAfterViewInit');
-  }
-
-  ngAfterViewChecked() {
-    console.log('ngAfterViewChecked');
-  }
-
-  ngOnDestroy() {
-    console.log('ngOnDestroy');
+    console.log(this.header.nativeElement.textContent);
   }
 
 }
